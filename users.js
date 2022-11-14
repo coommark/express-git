cdfb81023a6f64d56485ac6c4c5d137219c6e9ea
Mark Melton <coommark@gmail.com>
2022-11-14 19:08:04 +0100

    new modification

--- users.js
+++ users.js
@@ -34,7 +34,7 @@ router.get("/single", (req, res) => {
 router.get("/admin", (req, res) => {
     logger();
     res.json({
-        name: "Changed by Owner",
+        name: "Changed by Twice",
         career: "Programmer",
     });
 });
